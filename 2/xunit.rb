b1d85ac8e715b561558da9fde06f67487fa4708c
Kohei Yamamoto <kymmt90@gmail.com>
2017-12-21 23:43:09 +0900

    19

--- 2/xunit.rb
+++ 2/xunit.rb
@@ -1,6 +1,47 @@
-require_relative 'was_run'
+class TestCase
+  attr_reader :name
 
-test = WasRun.new('test_method')
-p test.was_run
-test.run
-p test.was_run
+  def initialize(name)
+    @name = name
+  end
+
+  def run
+    set_up
+    public_send(name)
+  end
+
+  def set_up
+  end
+end
+
+class WasRun < TestCase
+  attr_reader :was_run, :was_set_up
+
+  def set_up
+    @was_run = nil
+    @was_set_up = 1
+  end
+
+  def test_method
+    @was_run = 1
+  end
+end
+
+class TestCaseTest < TestCase
+  def set_up
+    @test = WasRun.new('test_method')
+  end
+
+  def test_running
+    @test.run
+    raise 'assert' if @test.was_run == nil
+  end
+
+  def test_set_up
+    @test.run
+    raise 'assert' unless @test.was_set_up
+  end
+end
+
+TestCaseTest.new('test_running').run
+TestCaseTest.new('test_set_up').run
