73f127e5361f5ed73c39e7fd06ac0add7cfbfd7d
Kohei Yamamoto <kymmt90@gmail.com>
2017-12-22 00:01:25 +0900

    20

--- 2/xunit.rb
+++ 2/xunit.rb
@@ -8,40 +8,39 @@ class TestCase
   def run
     set_up
     public_send(name)
+    tear_down
   end
 
   def set_up
+  end
+
+  def tear_down
   end
 end
 
 class WasRun < TestCase
-  attr_reader :was_run, :was_set_up
+  attr_reader :log
 
   def set_up
-    @was_run = nil
-    @was_set_up = 1
+    @log = __method__.to_s
+  end
+
+  def tear_down
+    @log << " #{__method__.to_s}"
   end
 
   def test_method
-    @was_run = 1
+    @log << " #{__method__.to_s}"
   end
 end
 
 class TestCaseTest < TestCase
-  def set_up
-    @test = WasRun.new('test_method')
-  end
-
-  def test_running
-    @test.run
-    raise 'assert' if @test.was_run == nil
-  end
-
-  def test_set_up
-    @test.run
-    raise 'assert' unless @test.was_set_up
+  def test_template_method
+    test = WasRun.new('test_method')
+    test.run
+    expect = 'set_up test_method tear_down'
+    raise "expect #{expect}, actual #{test.log}" unless test.log == expect
   end
 end
 
-TestCaseTest.new('test_running').run
-TestCaseTest.new('test_set_up').run
+TestCaseTest.new('test_template_method').run
